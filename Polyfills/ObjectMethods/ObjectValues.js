119a753ae7bc993eaa78c9e9442c911bfa018ec8
D Hareesh <71695825+Hareesh982@users.noreply.github.com>
2024-12-17 20:57:56 +0530

    Add files via upload

---
+++ Polyfills/ObjectMethods/ObjectValues.js
@@ -0,0 +1,21 @@
+let marks = {
+    john : 24,
+    mark : 25,
+    joey : 30,
+    ross : 50
+}
+
+Object.values = null
+if(!Object.values){
+    Object.values = function(obj){
+        let temp = []
+        for(let key in obj){
+            temp.push(obj[key])
+        }
+        return temp
+    }
+}
+console.log(Object.values(marks)); // [ 24, 25, 30, 50 ]
+
+
+
