71f0e05b7039bccb2ea11cfd581afc254eff3b70
D Hareesh <71695825+Hareesh982@users.noreply.github.com>
2024-12-17 20:50:11 +0530

    Delete PracticeAssignment directory

--- PracticeAssignment/program_18.js
+++
@@ -1,9 +0,0 @@
-// Write a function for input: ["1a1", "2aa3a", 'aabc31", "101", "1001", "a1", "3211", "11c"] and output [101, 1001, 3211]
-
-
-let arr = ["1a1", "2aa3a", "aabc31", "101", "1001", "a1", "3211", "11c"]
-
-console.log(arr.filter(str => !isNaN(Number(str))).map(num => Number(num)));
-
-
-// [ 101, 1001, 3211 ]
